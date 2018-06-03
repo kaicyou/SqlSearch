41e9f93ed4ac1cb2d65fdf6b91fdd8bc3bce25d9
wanghaox <kaicyouwhx@gmail.com>
2018-06-03 11:11:52 -0700

    fix bug and add comments

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 function searchInput() {
+	// google search prefx
 	var header = "https://www.google.com/search?q="
+	
+	// get data from checkbox
 	var option = document.getElementById("Options");
 	var strOpt = option.options[option.selectedIndex].value;
+	
+	// get data from textbox
 	var input = document.getElementById("SearchInput");
 	var strIpt = input.value;
+	
+	// concat string
 	var searchStr = strIpt != "" ? strOpt + '+' + strIpt : strOpt;
-	window.open(header + searchStr);
+	var wholeStr = header + searchStr;
+	
+	// start search
+	if (wholeStr != "")
+		window.open(header + searchStr);
 }
 
 function checkKey() {
+	// if press Enter key, do search
 	if (event.keyCode == 13)
 		searchInput()
 }
